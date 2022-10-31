21f0a3efceb862a09ac3fb68eaeedd027867c159
brenley-woodard <brenleyew@gmail.com>
2022-10-31 12:47:13 -0700

    incremented total guesses and created element for correct place in handleGuess function

--- app.js
+++ app.js
@@ -37,9 +37,11 @@ boulderButton.addEventListener('click', () => {
 
 function handleGuess(correctSpot, userGuess) {
     // reset the styles
-    resetStyles;
+    resetStyles();
     // then increment the guesses
+    totalGuesses++;
     // then grab the appropriate container element for the correct guess from the DOM
+    const correctPlaceEl = document.getElementById(`${}-container`);
     // then add the face class to that element so that the face shows up
     // then if the user guess is correct, increment the correct guesses
     // update the DOM to show this change to the user (including the losses, not tracked directly in state)
