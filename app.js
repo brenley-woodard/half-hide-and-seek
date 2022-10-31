6da3a0e7955b3c633f04bcc0cd8754e05b7d737d
brenley-woodard <brenleyew@gmail.com>
2022-10-31 12:10:48 -0700

    reset the styles

--- app.js
+++ app.js
@@ -37,9 +37,16 @@ boulderButton.addEventListener('click', () => {
 
 function handleGuess(correctSpot, userGuess) {
     // reset the styles
+    resetStyles;
     // then increment the guesses
     // then grab the appropriate container element for the correct guess from the DOM
     // then add the face class to that element so that the face shows up
     // then if the user guess is correct, increment the correct guesses
     // update the DOM to show this change to the user (including the losses, not tracked directly in state)
+}
+
+function resetStyles() {
+    shedContainer.classList.remove('face');
+    boulderContainer.classList.remove('face');
+    treeContainer.classList.remove('face');
 }
